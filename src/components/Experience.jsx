32ffd500bc87384a010cd60f2350b950f808af91
RajatSharma <136567058+rajatgithub2004@users.noreply.github.com>
2024-05-15 23:19:09 +0530

    Update Experience.jsx

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -89,7 +89,7 @@ function Experience() {
     },
   ]
   return (
-    <div name="experience" className='w-full'> 
+    <div name="experience" className='w-full mt-10'> 
       <div className='text-black max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full'>
         <div>
           <p className='text-4xl font-bold border-b-4 border-gray-500 p-2 inline'>Exprerience</p>
