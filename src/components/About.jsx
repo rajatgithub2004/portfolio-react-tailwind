8eaeb6d85aa485e921977503397df95831aa9095
RajatSharma <136567058+rajatgithub2004@users.noreply.github.com>
2024-05-16 11:14:44 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function About() {
   return (
-    <div name="about" className='w-full md:h-[50vh] h-[65vh] text-black'>
+    <div name="about" className='w-full md:h-[50vh] h-full text-black'>
       <div className='max-w-screen-lg p-4 mx-auto flex justify-center flex-col w-full'>
         <div className='pb-8'>
           <p className='text-4xl font-bold inline border-b-4 border-gray-600'>About Us</p>
