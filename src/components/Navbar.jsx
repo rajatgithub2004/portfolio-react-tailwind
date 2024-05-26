4eaba72594353f7a0bf3aeebb544a8b780748a3d
RajatSharma <136567058+rajatgithub2004@users.noreply.github.com>
2024-05-26 10:55:40 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -31,7 +31,7 @@ function Navbar() {
       <div >
         <h1 className='px-0 text-lg md:text-2xl md:ml-1 '>WebSphere INNOVATIONS</h1>
       </div>
-      <ul className='md:flex hidden mr-[11rem]'>
+      <ul className='md:flex hidden mr-[1rem]'>
       {links.map(({id,link}) =>(
           <li key={id} className='px-4 capitalize cursor-pointer hover:scale-105 text-xl'><Link to={link} smooth duration={500}>{link}</Link></li>
         ))}
@@ -40,7 +40,7 @@ function Navbar() {
         {nav? <FaTimes className='text-white' size={30} /> : <FaBars size={30} />}
       </div>
       {nav && (
-        <ul className='flex flex-col items-center absolute bg-gradient-to-b from-black to-gray-800 text-white top-[-30px] left-[-33px] w-full h-[60vh] justify-center  '>
+        <ul className='flex flex-col items-center absolute bg-gradient-to-b from-black to-gray-800 text-white top-[-30px] left-[-33px] w-[113vw] h-[60vh] justify-center  '>
         {links.map(({id,link}) =>(
             <li key={id} className='capitalize cursor-pointer hover:scale-105 text-xl py-6 '><Link onClick={() =>{setNav(!nav)}} to={link} smooth duration={500}>{link}</Link></li>
           ))}
