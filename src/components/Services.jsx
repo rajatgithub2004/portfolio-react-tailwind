81883b4b191390386cca1831bfe5cb363d6c3bad
RajatSharma <136567058+rajatgithub2004@users.noreply.github.com>
2024-05-26 10:56:04 +0530

    Update Services.jsx

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -5,37 +5,47 @@ import videoedit from '../assets/services/videoediting.jpg'
 
 function Services() {
   return (
-    <div name="services" className='w-full text-black md:h-screen'>
-      <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full '>
+    <div name="services" className='w-full text-black md:h-[24rem]'>
+      <div className='max-w-screen-lg  mx-auto flex flex-col justify-center w-full '>
         <div className='pb-8'>
           <p className='text-4xl font-bold inline border-b-4 border-gray-700'>Our Services</p>
         </div>
         <div className='grid grid-rows-3 gap-10 md:grid-cols-3 w-full'>
-        
-          <div className='shadow-md shadow-gray-600 rounded-lg'>
-            <img className='rounded-md duration-200 hover:scale-105 cursor-pointer' src={videoedit}  alt='graphicdesign' />
-            <div className='flex items-center flex-col justify-center'>
-              <h1 className='font-bold mb-3'>Video Editing</h1>
-              <p className="font-normal">A polished video edit boosts brand visibility, engages audiences effectively, and drives conversion. Our team has experience in editing videos for all businesses from startups to big brands.</p>
-            </div>
+
+        <div>
+          <div className='shadow-md h-2/4 shadow-gray-600 rounded-lg'>
+            <img className='h-full w-full rounded-md duration-200 hover:scale-105 cursor-pointer' src={videoedit}  alt='graphicdesign' />
           </div>
+          <div className='flex items-center flex-col justify-center'>
+            <h1 className='font-bold mb-3 mt-5'>Video Editing</h1>
+            <p className="font-normal">A polished video edit boosts brand visibility, engages audiences effectively, and drives conversion. Our team has experience in editing videos for all businesses from startups to big brands.</p>
+          </div>
+        </div>
+          
+          
         
-        
-          <div className='shadow-md shadow-gray-600 rounded-lg'>
-            <img className='rounded-md duration-200 hover:scale-105 cursor-pointer' src={graphicdesign}  alt='graphicdesign' />
+        <div>
+          <div className='shadow-md h-2/4 shadow-gray-600 rounded-lg'>
+            <img className='h-full w-full rounded-md duration-200 hover:scale-105 cursor-pointer' src={graphicdesign}  alt='graphicdesign' />
+          </div>
             <div className='flex items-center flex-col justify-center'>
-              <h1 className='font-bold mb-3'>Graphic Designing</h1>
+              <h1 className='font-bold mb-3 mt-5'>Graphic Designing</h1>
               <p className="font-normal">We are your one-stop solution for your complete graphic design requirements. Uplift your brand's perception through our unmatched creative solutions. </p>
             </div>
+        </div>
+          
+          
+          <div>
+          <div className='shadow-md h-2/4 shadow-gray-600 rounded-lg'>
+            <img className='h-full w-full rounded-md duration-200 hover:scale-105 cursor-pointer' src={webdev}  alt='graphicdesign' />
           </div>
-        
-          <div className='shadow-md shadow-gray-600 rounded-lg'>
-            <img className='rounded-md duration-200 hover:scale-105 cursor-pointer' src={webdev}  alt='graphicdesign' />
             <div className='flex items-center flex-col justify-center'>
-              <h1 className='mb-3 font-bold'>Website Building</h1>
+              <h1 className='mb-3 font-bold mt-5'>Website Building</h1>
               <p className="font-normal">We build exceptional web experiences making the best use of technology and innovation. Our team has experience in developing websites for all businesses from startups to big brands.</p>
             </div>
           </div>
+          
+          
         
         </div>
       </div>
