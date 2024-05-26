ab698ce4f76d54e454951519b15669cb6cbf4c17
RajatSharma <136567058+rajatgithub2004@users.noreply.github.com>
2024-05-26 10:55:16 +0530

    Update Experience.jsx

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -89,15 +89,15 @@ function Experience() {
     },
   ]
   return (
-    <div name="experience" className='w-full mt-10'> 
+    <div name="experience" className='w-full'> 
       <div className='text-black max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full'>
         <div>
-          <p className='text-4xl font-bold border-b-4 border-gray-500 p-2 inline'>Experience</p>
+          <p className='text-4xl font-bold border-b-4 border-gray-500 p-2 inline'>Exprerience</p>
           <p className='py-6'>Technologies our team can work with</p>
         </div>
         <div className='w-full grid grid-col-2 sm:grid-cols-4 gap-8 text-center py-8 px-12 sm:px-0'> 
           {tech.map(({id,src,title,style})=>(
-            <div key={id} className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}>
+            <div key={id} className={`shadow-md flex flex-col justify-end hover:scale-105 duration-500 py-2 rounded-lg ${style}`}>
             <img className='w-20 mx-auto' src={src} />
             <p className='mt-4'>{title}</p>
           </div>
